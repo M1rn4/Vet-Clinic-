0c662b92dd6242b690a6da60d86909bdf230a2da
Mirna <ampueromirna8@gmail.com>
2023-04-03 12:17:03 -0500

    query multiple tables

--- data.sql
+++ data.sql
@@ -18,4 +18,43 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, 
 ('Blossom', '1998-10-13', 3, true, 17, 'unknown'),
 ('Ditto', '2022-05-14', 4, true, 22, 'unknown');
 
+/*add new information day 3*/
+INSERT INTO owners (full_name, age) VALUES 
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+-- Update animals table with owner information
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Sam Smith'
+)
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Jennifer Orwell'
+)
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Bob'
+)
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Melody Pond'
+)
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Dean Winchester'
+)
+WHERE name IN ('Angemon', 'Boarmon');
 
--- schema.sql
+++ schema.sql
@@ -9,3 +9,26 @@ CREATE TABLE animals (
   weight_kg DECIMAL(10,2)
 );
 
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+-- Modify the id column to be autoincremented and set as primary key
+ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
+
+-- Remove the species column
+ALTER TABLE animals DROP COLUMN IF EXISTS species;
+
+-- Add the species_id column as a foreign key referencing the species table
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+-- Add the owner_id column as a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
